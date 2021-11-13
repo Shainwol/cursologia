73c74122296fb655058e0f11e498ff0aaea595ee
Shainwol <72896302+Shainwol@users.noreply.github.com>
2021-11-13 14:28:55 -0600

    Update footer.php

--- Views/Template/footer.php
+++ Views/Template/footer.php
@@ -6,20 +6,12 @@
 		<div class="Vacio"></div>
 
 		<footer class="bg-dark text-center text-white">
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
-    	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="<?= media(); ?>js/<?= $data['page_functions_js']?>"></script>	
-			
-            <!-- Copyright -->
-            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-              © 2020 Copyright:
-              <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-            </div>
-            <!-- Copyright -->
-    </footer>
-
-    
+		    <!-- Copyright -->
+		    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+		      © 2020 Copyright:
+		      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+		    </div>
+		    <!-- Copyright -->
+    		</footer>
   </body>
 </html>
