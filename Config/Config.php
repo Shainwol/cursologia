f763feb8ad48434a39d3d85bd9d602ae5aefe48d
Shainwol <72896302+Shainwol@users.noreply.github.com>
2021-11-13 13:58:31 -0600

    config

---
+++ Config/Config.php
@@ -0,0 +1,14 @@
+<?php 
+
+	const BASE_URL = "http://localhost:8080/dashboard/PaginaCursos/";
+
+	date_default_timezone_set('America/Monterrey');
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+
+?>
